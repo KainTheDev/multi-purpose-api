738152fa015407a87ed0c8e639679872b752e44c
KainTheDev <thica.doi@gmail.com>
2024-01-04 13:39:51 +0700

    .

--- functions/allEndpoints.js
+++
@@ -1,6 +0,0 @@
-const { readdirSync } = require("fs")
-
-module.exports = (req, res) => {
-    const endpoints = readdirSync('endpoints').map(endpoint => endpoint.split(".")[0])
-    res.json({endpoints: endpoints})
-}
