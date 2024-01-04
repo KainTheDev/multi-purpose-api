5048f7ddd51e0a1d462e4636257fe9814c6fecab
KainTheDev <thica.doi@gmail.com>
2024-01-04 13:31:55 +0700

    .

--- node_modules/multi-purpose/utils/randomWUR.js
+++
@@ -1,8 +0,0 @@
-const { default: fetch } = require('node-fetch');
-/**
- * @returns {JSON}
- */
-async function randomWUR() {
-  return await (await fetch('https://api.truthordarebot.xyz/v1/truth')).json()
-}
-module.exports = randomWUR;
